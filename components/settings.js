fe49f29ce7158528853b5ff60affe6b6b54206f8
John Clary <john.clary@austintexas.gov>
2022-06-28 10:14:22 -0400

    use settings file

---
+++ components/settings.js
@@ -0,0 +1,8 @@
+export const ISSUES_ENDPOINT = encodeURI(
+  "https://data.austintexas.gov/resource/rzwg-fyv8.json?$limit=100000&$where=labels like '%Project Index%' or labels like '%Product Index%'"
+);
+
+export const EVALUATIONS_ENDPOINT =
+  "https://api.knack.com/v1/pages/scene_162/views/view_311/records";
+
+export const EVALS_APP_ID = "595d00ebd315cc4cb98daff4";
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import { IssuesContextWrapper } from "../components/wrappers/IssuesContextWrapper";
 import { EvaluationsContextWrapper } from "../components/wrappers/EvaluationsContextWrapper";
 import CustomHead from "./CustomHead";
+import {
+  ISSUES_ENDPOINT,
+  EVALUATIONS_ENDPOINT,
+  EVALS_APP_ID,
+} from "../components/settings";
 import "../styles/custom.scss";
-
-const ISSUES_ENDPOINT =
-  "https://data.austintexas.gov/resource/rzwg-fyv8.json?$limit=100000";
-
-const EVALUATIONS_ENDPOINT =
-  "https://api.knack.com/v1/pages/scene_162/views/view_311/records";
-
-const EVALS_APP_ID = "595d00ebd315cc4cb98daff4";
 
 function App({ Component, pageProps }) {
   return (
