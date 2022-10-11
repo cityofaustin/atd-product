e518c82beb6d2077859f1b2aebd0d8476fe4070f
John Clary <john.clary@austintexas.gov>
2022-10-11 11:53:18 -0400

    cleaning up imports

--- pages/products/[issue_number]/index.js
+++ pages/products/[issue_number]/index.js
@@ -1,14 +1,12 @@
-import React from "react";
+import { useContext } from "react";
 import { useRouter } from "next/router";
 import IssuesContext from "../../../contexts/IssuesContext";
 import IndexIssueDetails from "../../../components/shared/indexIssueDetails/IndexIssueDetails";
 import Page from "../../../components/shared/Page";
 
 export default function ProductDetailsView() {
-  const context = React.useContext(IssuesContext);
-  const { issues, isLoaded, error } = context;
+  const { issues, isLoaded, error } = useContext(IssuesContext);
   const matches = useRouter("/products/:issue_number");
-
   const issue_number = matches.query.issue_number;
   const issue = issues.find((issue) => issue.number === issue_number);
   return (
