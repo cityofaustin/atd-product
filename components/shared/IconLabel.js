df31c62ff02878ca4d73e21f314fb9f7547e664e
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-10-12 15:08:12 -0500

    adjust classname for bootstrap v4

--- components/shared/IconLabel.js
+++ components/shared/IconLabel.js
@@ -5,7 +5,7 @@ export default function IconLabel({ Icon, label }) {
   return (
     <div className="d-flex align-items-center">
       <Icon />
-      <span className="ms-1">{label}</span>
+      <span className="ml-1">{label}</span>
     </div>
   );
 }
