d6a651abd674cef46f17dbb3a6e3df09a784b140
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-10-24 17:14:58 -0500

    remove underline and correct color for link

--- components/shared/indexIssueDetails/IndexIssueDetails.js
+++ components/shared/indexIssueDetails/IndexIssueDetails.js
@@ -133,6 +133,7 @@ function InfoRow({ issue }) {
         <h6 className="mb-0 mt-2 text-muted">Issue</h6>
         <a
           href={`https://github.com/cityofaustin/atd-data-tech/issues/${issue.number}`}
+          className="text-primary text-decoration-none"
         >{`#${issue.number}`}</a>
       </Col>
     </Row>
