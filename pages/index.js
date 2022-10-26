647ef8678729dbdcd0f51212e08dfe06afab3a3b
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-10-26 17:03:48 -0500

    update homepage with new hero section & body, start on carousel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
-import { Row, Col, Card, Image } from "react-bootstrap";
-import { FaGlobe, FaUsersCog, FaBusinessTime } from "react-icons/fa";
+import { Row, Col, Card, Carousel } from "react-bootstrap";
+import { IoIosPeople } from "react-icons/io";
+import { FaGlobe, FaBriefcase } from "react-icons/fa";
 import { MdWeb } from "react-icons/md";
 import IssuesContext from "../contexts/IssuesContext";
 import Page from "../components/shared/Page";
@@ -9,16 +10,12 @@ import IndexIsssueListItem from "../components/shared/IndexIssueListItem";
 function ServiceItem(props) {
   const { icon, content } = props;
   return (
-    <Col md={3} className="pb-4">
+    <Col>
       <Row>
-        <Col xs="auto" className="pe-0">
-          <span style={{ color: "#00bfa5" }}>
-            <h1>{icon}</h1>
-          </span>
-        </Col>
-        <Col>
-          <h5>{content}</h5>
-        </Col>
+        <h1>{icon}</h1>
+      </Row>
+      <Row>
+        <h5 className="fw-normal">{content}</h5>
       </Row>
     </Col>
   );
@@ -36,83 +33,58 @@ export default function HomeView() {
             <Card.ImgOverlay>
               <Card.Body>
                 <Card.Title className="pt-5 text-center text-white fw-bold">
-                  <h1 className="fw-bold">Tech Support for ATX Mobility</h1>
+                  <h1 className="pt-4 fw-bold" style={{ fontSize: "3.5rem" }}>
+                    Tech Support for ATX Mobility
+                  </h1>
                 </Card.Title>
-                <Card.Text className="text-center text-white">
-                  <h3>
+                <Card.Text className="pt-4 text-center text-white">
+                  <p style={{ fontSize: "1.75rem", fontWeight: 500 }}>
                     We build and buy technology to help city staff <br></br>
                     tackle Austin&apos;s mobility challenges.
-                  </h3>
+                  </p>
                 </Card.Text>
               </Card.Body>
             </Card.ImgOverlay>
           </Card>
         </Row>
-        <Row
-          className="py-3 text-primary"
-          style={{
-            backgroundColor: "#f8f9fa",
-          }}
-        >
-          <Col md={2}></Col>
-          <Col>
-            <h2>
-              <strong>We&apos;re here to help</strong>
-            </h2>
-          </Col>
+        <Row className="py-5 text-primary text-center">
+          <h2>
+            <strong>We&apos;re here to help</strong>
+          </h2>
         </Row>
-        <Row
-          className="text-primary"
-          style={{
-            backgroundColor: "#f8f9fa",
-          }}
-        >
-          <Col md={2}></Col>
+        <Row className="text-primary text-center">
+          <Col sm={1}></Col>
           <ServiceItem
             content="Modernize software development processes while introducing good agile and human-centered design practices"
-            icon={<FaUsersCog style={{ verticalAlign: "top" }} />}
+            icon={<IoIosPeople style={{ verticalAlign: "top" }} />}
           />
-          <Col md={2}></Col>
-          <ServiceItem
-            content="Digitize and streamline internal systems to save time and unlock insights from data"
-            icon={<FaBusinessTime style={{ verticalAlign: "top" }} />}
-          />
-          <Col md={2}></Col>
-        </Row>
-        <Row
-          className="pb-4 text-primary"
-          style={{
-            backgroundColor: "#f8f9fa",
-          }}
-        >
-          <Col md={2}></Col>
           <ServiceItem
             content="Improve public-facing services like websites or applications"
             icon={<MdWeb style={{ verticalAlign: "top" }} />}
           />
-          <Col md={2}></Col>
+          <ServiceItem
+            content="Digitize and streamline internal systems to save time and unlock insights from data"
+            icon={<FaBriefcase style={{ verticalAlign: "top" }} />}
+          />
           <ServiceItem
             content="Manage geospatial data and publish it on the web"
             icon={<FaGlobe style={{ verticalAlign: "top" }} />}
           />
-          <Col md={2}></Col>
+          <Col sm={1}></Col>
         </Row>
+        <hr className="my-3 mx-4"></hr>
         <Row className="pt-3 pb-4 text-primary">
-          <Col md={2}></Col>
+          <Col sm={1}></Col>
           <Col>
-            <h2>
-              <strong>Featured projects</strong>
-            </h2>
+            <h4 className="fw-bold">Featured projects</h4>
           </Col>
         </Row>
         <Row>
-          {projects.map((project) => {
-            return project.isFeatured ? (
-              <Col key={project.number} md={3} className="m-0 p-2">
-                <IndexIsssueListItem type="project" issue={project} />
-              </Col>
-            ) : null;
-          })}
+          <Carousel>
+            <Carousel.Item></Carousel.Item>
+
+            <Carousel.Item></Carousel.Item>
+          </Carousel>
         </Row>
       </Page>
     </>
