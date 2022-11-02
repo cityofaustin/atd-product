924c9f283c2d191caa25e6bc741ab059b52432f6
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-11-02 18:06:27 -0500

    make arrows disappear on slide, fix import typo

--- components/FeaturedCarousel.js
+++ components/FeaturedCarousel.js
@@ -1,12 +1,16 @@
-import { useContext } from "react";
-import IssuesContext from "../contexts/IssuesContext";
-import IndexIsssueListItem from "../components/shared/IndexIssueListItem";
-import { Row, Col, Carousel } from "react-bootstrap";
+import { useContext, useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
 import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import IssuesContext from "../contexts/IssuesContext";
+import IndexIssueListItem from "../components/shared/IndexIssueListItem";
 
 export default function FeaturedCarousel() {
-  const { issues } = useContext(IssuesContext);
+  // Set state for next/previous arrows so they disappear on slide
+  const [showArrows, setShowArrows] = useState(true);
 
+  const { issues } = useContext(IssuesContext);
   const featuredIssues = issues.filter((issue) => issue.isFeatured);
 
   return (
@@ -16,17 +20,22 @@ export default function FeaturedCarousel() {
       nextIcon={<IoIosArrowDropright className="fs-1 text-primary" />}
       prevIcon={<IoIosArrowDropleft className="fs-1 text-primary" />}
       interval={null}
+      controls={showArrows}
+      onSlide={(e) => setShowArrows(false)}
+      onSlid={(e) => setShowArrows(true)}
     >
       <Carousel.Item>
         <Row className="mb-3">
           <Col sm={1}></Col>
+          {/* We have decided that at most 8 featured projects will ever be
+          displayed */}
           {featuredIssues.slice(0, 4).map((issue) => {
             const type = issue.labels.includes("Product Index")
               ? "product"
               : "project";
             return (
               <Col key={issue.number}>
-                <IndexIsssueListItem issue={issue} type={type} />
+                <IndexIssueListItem issue={issue} type={type} />
               </Col>
             );
           })}
@@ -42,7 +51,7 @@ export default function FeaturedCarousel() {
               : "project";
             return (
               <Col key={issue.number}>
-                <IndexIsssueListItem issue={issue} type={type} />
+                <IndexIssueListItem issue={issue} type={type} />
               </Col>
             );
           })}
