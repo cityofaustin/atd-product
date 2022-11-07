687939f7abb3e7acd642a98d3e5c9ccefa7fd30c
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-11-07 10:47:54 -0600

    import large breakpoint const for mediaquery, remove card padding for better mobile experience, change sm breakpoints to xs, remove e argument in callbacks

--- components/FeaturedSection.js
+++ components/FeaturedSection.js
@@ -6,6 +6,7 @@ import { useMediaQuery } from "react-responsive";
 import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
 import IssuesContext from "../contexts/IssuesContext";
 import IndexIssueListItem from "./shared/IndexIssueListItem";
+import { LARGE_BREAKPOINT } from "./settings";
 
 export default function FeaturedSection() {
   // Set state for next/previous arrows so they disappear on slide
@@ -14,18 +15,20 @@ export default function FeaturedSection() {
   const { issues } = useContext(IssuesContext);
   const featuredIssues = issues.filter((issue) => issue.isFeatured);
 
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 992px)" });
+  const isTabletOrMobile = useMediaQuery({
+    query: `(max-width: ${LARGE_BREAKPOINT})`,
+  });
 
   return (
     <Row>
       {/* Render rows with featured cards if tablet or mobile else render carousel */}
       {isTabletOrMobile ? (
-        <Col className="mx-5">
+        <Col>
           <Row>
             {/* This slice is incase there are more than 8 issues with the featured label */}
             {featuredIssues.slice(0, 8).map((issue) => {
               return (
-                <Col s={12} md={6} key={issue.number} className="px-4 pb-5">
+                <Col xs={12} md={6} key={issue.number} className="px-4 pb-5">
                   <IndexIssueListItem issue={issue} />
                 </Col>
               );
@@ -33,7 +36,7 @@ export default function FeaturedSection() {
           </Row>
         </Col>
       ) : (
-        <Col className="px-0">
+        <Col>
           <Carousel
             variant="dark"
             indicators={false}
@@ -41,12 +44,12 @@ export default function FeaturedSection() {
             prevIcon={<IoIosArrowDropleft className="fs-1 text-primary" />}
             interval={null}
             controls={showArrows}
-            onSlide={(e) => setShowArrows(false)}
-            onSlid={(e) => setShowArrows(true)}
+            onSlide={() => setShowArrows(false)}
+            onSlid={() => setShowArrows(true)}
           >
             <Carousel.Item>
               <Row className="mb-3">
-                <Col sm={1}></Col>
+                <Col xs={1}></Col>
                 {/* We have decided that at most 8 featured projects will ever be displayed */}
                 {featuredIssues.slice(0, 4).map((issue) => {
                   return (
@@ -55,12 +58,12 @@ export default function FeaturedSection() {
                     </Col>
                   );
                 })}
-                <Col sm={1}></Col>
+                <Col xs={1}></Col>
               </Row>
             </Carousel.Item>
             <Carousel.Item>
               <Row className="mb-3">
-                <Col sm={1}></Col>
+                <Col xs={1}></Col>
                 {featuredIssues.slice(4, 8).map((issue) => {
                   return (
                     <Col key={issue.number}>
@@ -68,7 +71,7 @@ export default function FeaturedSection() {
                     </Col>
                   );
                 })}
-                <Col sm={1}></Col>
+                <Col xs={1}></Col>
               </Row>
             </Carousel.Item>
           </Carousel>
