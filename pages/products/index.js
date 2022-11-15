acde44ee792e1807d7b438c10c968385ab1ce5ad
Rose <67130963+roseeichelmann@users.noreply.github.com>
2022-11-15 11:34:06 -0600

    change title, description, add gray separation line

--- pages/products/index.js
+++ pages/products/index.js
@@ -7,13 +7,14 @@ import ProductsList from "../../components/productsView/ProductsList";
 
 function FrontMatter() {
   return (
-    <Col className="ms-2 mb-2 border-start border-secondary">
-      <p className="mb-0 ms-3">
-        <strong>Products</strong> are the solutions we build for our customers,
-        including Knack apps, AMANDA apps, custom software, and data systems. We
-        improve and extend our products over time so that they deliver
-        continuous value to Austin Transportation as business needs evolve.
+    <Col>
+      <p className="text-primary">
+        Products are the solutions we build for our customers, including Knack
+        apps, AMANDA apps, custom software, and data systems. We improve and
+        extend our products over time so that they deliver continuous value to
+        Austin Transportation as business needs evolve.
       </p>
+      <hr className="mt-4" />
     </Col>
   );
 }
@@ -22,7 +23,7 @@ export default function ProductsView() {
   const { issues, productIssues, isLoaded, error } = useContext(IssuesContext);
 
   return (
-    <Page nav title="What we deliver">
+    <Page nav title="Products">
       <Row key="front-matter" className="mb-2">
         <FrontMatter />
       </Row>
