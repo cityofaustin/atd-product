09fe176f6ad619db096edcf6aefd8570083de027
Rachel Wang <yixingw0716@gmail.com>
2023-08-01 15:02:32 -0700

    update URL with view=chart parameter

--- components/projectsView/ProjectListToggle.js
+++ components/projectsView/ProjectListToggle.js
@@ -2,9 +2,33 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import { FaChartArea } from "react-icons/fa";
 import { FaList } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 export default function ProjectListToggle(props) {
   const { showChartView, setShowChartView } = props;
+  const router = useRouter();
+
+  const switchToChartView = () => {
+    if (!showChartView) {
+      setShowChartView(true);
+      // Add the view=chart parameter to the URL
+      router.push(
+        { query: { ...router.query, view: "chart" } },
+        undefined,
+        { shallow: true }
+      );
+    }
+  };
+
+  const switchToListView = () => {
+    if (showChartView) {
+      setShowChartView(false);
+      // Remove the view=chart parameter from the URL
+      const { view, ...queryWithoutView } = router.query;
+      router.push({ query: queryWithoutView }, undefined, { shallow: true });
+    }
+  };
+
   return (
     <>
       <Col
@@ -14,7 +38,7 @@ export default function ProjectListToggle(props) {
             ? "rounded-start border-end-0 chart-toggle-inactive"
             : "rounded-start chart-toggle-active"
         }
-        onClick={() => setShowChartView(false)}
+        onClick={switchToListView}
       >
         <p className="text-center my-auto p-2">
           <FaList style={{ verticalAlign: "middle" }} /> List
@@ -27,7 +51,7 @@ export default function ProjectListToggle(props) {
             ? "rounded-end chart-toggle-active"
             : "rounded-end border-start-0 chart-toggle-inactive"
         }
-        onClick={() => setShowChartView(true)}
+        onClick={switchToChartView}
       >
         <p className="text-center my-auto p-2">
           <FaChartArea style={{ verticalAlign: "middle" }} /> Chart
@@ -36,3 +60,4 @@ export default function ProjectListToggle(props) {
     </>
   );
 }
+
