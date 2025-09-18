eadd6fb385f744712ac11fa17397c6abf12fb3f8
John Clary <john.clary@austintexas.gov>
2025-09-18 12:08:35 -0400

    try this path check hack

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { IssuesContextWrapper } from "../components/wrappers/IssuesContextWrapper";
 import { EvaluationsContextWrapper } from "../components/wrappers/EvaluationsContextWrapper";
 import CustomHead from "./CustomHead";
@@ -5,6 +7,34 @@ import { EVALUATIONS_ENDPOINT, EVALS_APP_ID } from "../components/settings";
 import "../styles/custom.scss";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [isCorrectRoute, setIsCorrectRoute] = useState(false);
+  useEffect(() => {
+    const path = window.location.pathname;
+
+    // If we're not on the homepage but router thinks we are
+    if (path !== "/" && router.pathname === "/") {
+      console.log("Route mismatch detected, forcing correction...");
+
+      // Push to the actual path
+      router
+        .push(path + window.location.search + window.location.hash)
+        .then(() => {
+          setIsCorrectRoute(true);
+        });
+    } else {
+      setIsCorrectRoute(true);
+    }
+  }, []);
+
+  // Don't render the wrong component
+  if (
+    !isCorrectRoute &&
+    window.location.pathname !== "/" &&
+    router.pathname === "/"
+  ) {
+    return <div>Loading correct page...</div>;
+  }
   return (
     <IssuesContextWrapper>
       <EvaluationsContextWrapper
