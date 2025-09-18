372edecaacd0bc6e583e9e31978ec670d842c476
John Clary <john.clary@austintexas.gov>
2025-09-18 11:53:59 -0400

    add logging to project deets

--- pages/projects/[issue_number]/index.js
+++ pages/projects/[issue_number]/index.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
 import IssuesContext from "../../../contexts/IssuesContext";
 import IndexIssueDetails from "../../../components/shared/indexIssueDetails/IndexIssueDetails";
@@ -9,6 +9,15 @@ export default function ProjectDetailsView() {
   const matches = useRouter();
   const issue_number = matches.query.issue_number;
   const issue = issues.find((issue) => issue.number === issue_number);
+  useEffect(() => {
+    console.log("Router debug:", {
+      isReady: router.isReady,
+      asPath: router.asPath,
+      pathname: router.pathname,
+      query: router.query,
+      windowLocation: window.location.pathname,
+    });
+  }, [router.isReady, router.asPath, router.query]);
   return (
     <>
       <Page nav title={false}>
