f1633d76832f6c5ce3edaffff2dde9c113bbb266
John Clary <john.clary@austintexas.gov>
2025-09-18 13:47:58 -0400

    remove these config changes that did nothing

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  trailingSlash: true,
-  experimental: {
-    strictNextHead: false,
-  },
-  // Force Next.js to generate all pages as separate HTML files
-  generateBuildId: async () => {
-    // This can help with route detection
-    return "static-build";
-  },
 };
 
 module.exports = nextConfig;
