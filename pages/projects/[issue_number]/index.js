719d37edeff252393a0555a936435812c65453eb
John Clary <john.clary@austintexas.gov>
2025-09-18 13:48:16 -0400

    remove debug logging effect

--- pages/projects/[issue_number]/index.js
+++ pages/projects/[issue_number]/index.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useRouter } from "next/router";
 import IssuesContext from "../../../contexts/IssuesContext";
 import IndexIssueDetails from "../../../components/shared/indexIssueDetails/IndexIssueDetails";
@@ -9,15 +9,6 @@ export default function ProjectDetailsView() {
   const matches = useRouter();
   const issue_number = matches.query.issue_number;
   const issue = issues.find((issue) => issue.number === issue_number);
-  useEffect(() => {
-    console.log("Router debug:", {
-      isReady: matches.isReady,
-      asPath: matches.asPath,
-      pathname: matches.pathname,
-      query: matches.query,
-      windowLocation: window.location.pathname,
-    });
-  }, [matches.isReady, matches.asPath, matches.query]);
   return (
     <>
       <Page nav title={false}>
