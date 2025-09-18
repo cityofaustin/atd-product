14b3f875e2692fb34bdd5cfc327d18bf6d562150
John Clary <john.clary@austintexas.gov>
2025-09-18 13:48:47 -0400

    tighten up and document the router patch

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,23 @@ import "../styles/custom.scss";
 function App({ Component, pageProps }) {
   const router = useRouter();
   const [isCorrectRoute, setIsCorrectRoute] = useState(false);
-  useEffect(() => {
-    const path = window.location.pathname;
 
+  useEffect(() => {
+    /**
+     * See https://github.com/cityofaustin/atd-data-tech/issues/24354
+     *
+     * Upgrading to Next v15 (current: v15.5.3) broke our dynamic page
+     * routing. This effect fixes it by manually checking if there is
+     * a mismatch between the current current window.location and the
+     * page that the next/router thinks we're on. If so, the page
+     * is router manually directed to the correct path.
+     */
+    if (isCorrectRoute) return;
+
+    const path = window.location.pathname;
     // If we're not on the homepage but router thinks we are
     if (path !== "/" && router.pathname === "/") {
       console.log("Route mismatch detected, forcing correction...");
-
       // Push to the actual path
       router
         .push(path + window.location.search + window.location.hash)
@@ -25,16 +35,16 @@ function App({ Component, pageProps }) {
     } else {
       setIsCorrectRoute(true);
     }
-  }, []);
+  }, [router, isCorrectRoute]);
 
-  // Don't render the wrong component
+  // Don't render until we're on the correct route
   if (
     !isCorrectRoute &&
     typeof window !== "undefined" &&
     window.location.pathname !== "/" &&
     router.pathname === "/"
   ) {
-    return <div>Loading correct page...</div>;
+    return <div>Loading...</div>;
   }
   return (
     <IssuesContextWrapper>
