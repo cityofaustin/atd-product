6af2f739bc7efa9c14ca5ce40099a52cde5074d4
John Clary <john.clary@austintexas.gov>
2025-09-18 12:03:01 -0400

    try expirmental app router suppressor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-   trailingSlash: true,
+  trailingSlash: true,
+  experimental: {
+    // Force client-side navigation for static export
+    clientRouterFilter: false,
+  },
 };
 
 module.exports = nextConfig;
